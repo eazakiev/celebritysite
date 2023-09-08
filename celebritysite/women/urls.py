6f026bd67f98014843eb6e56076edc11d15800bd
Evgeny <zakiev_e@mail.ru>
2023-09-08 16:02:39 +0300

    add functionality

--- celebritysite/women/urls.py
+++ celebritysite/women/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-# app_name = "women"
 urlpatterns = [
     path('', views.index, name='home'),
     path("about/", views.about, name="about"),
     path("addpage/", views.addpage, name="add_page"),
     path("contact/", views.contact, name="contact"),
     path("login/", views.login, name="login"),
-    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('post/<slug:post_slug>/', views.show_post, name='post'),
+    path('category/<slug:cat_slug>/', views.show_category, name='category'),
 ]
--- celebritysite/women/views.py
+++ celebritysite/women/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Women
+from .models import Category, Women
 
 menu = [{'title': "О сайте", 'url_name': "about"},
         {'title': "Добавить статью", 'url_name': "add_page"},
@@ -11,17 +11,15 @@ menu = [{'title': "О сайте", 'url_name': "about"},
 
 
 def index(request):
-    posts = Women.objects.all()
+    posts = Women.objects.filter(is_published=True)
+    # posts = Women.objects.all()
     context = {
         "posts": posts,
         "menu": menu,
         "title": "Главная страница",
+        "cat_selected": 0,
     }
     return render(request, "women/index.html", context=context)
-
-# def index(request):
-    # posts = Women.objects.all()
-    # return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
 
 
 def about(request):
@@ -35,6 +33,7 @@ def about(request):
 def addpage(request):
     return HttpResponse("Добавление статьи")
 
+
 def contact(request):
     return HttpResponse("Обратная связь")
 
@@ -44,8 +43,30 @@ def login(request):
 
 
 def pageNotFound(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+    return HttpResponseNotFound("<h1>Страница не найдена (Боевой сервер!)</h1>")
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        "post": post,
+        "menu": menu,
+        "title": post.title,
+        "cat_selected": post.cat_id,
+    }
+    return render(request, 'women/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        "posts": posts,
+        "menu": menu,
+        "title": "Отображение по рубрикам",
+        "cat_selected": cat_slug
+    }
+    return render(request, "women/index.html", context=context)
